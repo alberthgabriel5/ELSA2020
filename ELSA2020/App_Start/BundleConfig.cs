019b41a4b5760eff3b0a44ca3e049fbfada4336c
alberthgabriel5 <alberth3_5@hotmail.com>
2020-04-16 19:09:10 -0600

    Commit 1 Incompleto con template pero utilizable

--- ELSA2020/App_Start/BundleConfig.cs
+++ ELSA2020/App_Start/BundleConfig.cs
@@ -5,26 +5,45 @@ namespace ELSA2020
 {
     public class BundleConfig
     {
-        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+       
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryslicknav").Include(
+                       "~/Scripts/jquery.slicknav.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquerymagnific").Include(
+                        "~/Scripts/jquery.magnific-popup.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                        "~/Scripts/main.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/isotope").Include(
+                        "~/Scripts/isotope.pkgd.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/masonry").Include(
+                        "~/Scripts/masonry.pkgd.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/carousel").Include(
+                        "~/Scripts/owl.carousel.min.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/map").Include(
+                      "~/Content/style.css.map"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/elegant-icons.css",
+                      "~/Content/flaticon.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/magnific-popup.css",                      
+                      "~/Content/slicknav.min.css",
+                      "~/Content/style.css"));
         }
     }
 }
