e64c5a19a65740c93c2e027f272bb715fb70722a
Eyleen <eyleenqh@hotmail.com>
2020-04-17 14:50:39 -0600

    Historia 2

--- ELSA2020/Controllers/HomeController.cs
+++ ELSA2020/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace ELSA2020.Controllers
     {
         public ActionResult Index()
         {
+            ViewData["texto"] = "Hotel colibri, es un lugar rodeado de montañas, hermosas vistas, arboles, vegetacion hermosa, aves y animales silvestres lo cuales se pueden apreciar en los recorrido atravez de los senderos de los alrededores del hotel, cuenta con varios servicios a los cuales podra accesar durante su estadia en nuestro hotel.";
+            ViewData["imagen"] = "~/img/paginainicio/hotel.jpg";
             return View();
         }
 
