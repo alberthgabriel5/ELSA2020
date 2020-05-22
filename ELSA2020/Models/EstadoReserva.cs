1bd987a7cd9c7a9c8c0debfb2043407208a5acc9
LuisCa2492 <48541286+LuisCa2492@users.noreply.github.com>
2020-05-22 12:00:29 -0600

    aaa

---
+++ ELSA2020/Models/EstadoReserva.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ELSA2020.Models
+{
+    public class EstadoReserva
+    {
+        public int numeroHabitacion { get; set; }
+        public string tipo { get; set; }
+        public string estado { get; set; }
+    }
+}
